1f3ca3a016a53b32a20ac13c9205ae8a6b85ac84
NikoDemo96 <chnicolas.aranguiz@gmail.com>
2023-02-28 18:49:02 +0000

    "cambios"

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+console.log("Probando probando");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["la", "el"];
+let adj = ["mejor", "masmejor"];
+let noun = ["pagina", "dominio"];
+let extension = [".com", ".io"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(pronoun);
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let u = 0; u < extension.length; u++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + extension[u]);
+      }
+    }
+  }
+}
